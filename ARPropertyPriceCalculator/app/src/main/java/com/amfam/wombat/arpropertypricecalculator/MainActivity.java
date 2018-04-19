fd6015d02e7386847c233e9f11f3411012bd4942
Leeladhar Bandi <LXB051@amfam.com>
2018-04-19 17:21:08 -0500

    add howToUse.xml and .java

--- ARPropertyPriceCalculator/app/src/main/java/com/amfam/wombat/arpropertypricecalculator/MainActivity.java
+++ ARPropertyPriceCalculator/app/src/main/java/com/amfam/wombat/arpropertypricecalculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.amfam.wombat.arpropertypricecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user clicks the Next button on home screen */
+    public void goToScreen2(View view) {
+        // Do something in response to button
+        Button button = (Button) view;
+        ((Button) view).setText("clicked");
+    }
+
 }
