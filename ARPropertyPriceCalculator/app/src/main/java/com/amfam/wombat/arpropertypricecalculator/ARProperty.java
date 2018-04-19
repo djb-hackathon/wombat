74544fe7d5d3d3dcb11568cb959f4c2af4ecf0ce
rab057 <ryan.behr@amfam.com>
2018-04-19 17:16:05 -0500

    Datatype which can be used for storing property info and displaying it on the UI.

---
+++ ARPropertyPriceCalculator/app/src/main/java/com/amfam/wombat/arpropertypricecalculator/ARProperty.java
@@ -0,0 +1,9 @@
+package com.amfam.wombat.arpropertypricecalculator;
+
+import java.io.File;
+
+public class ARProperty {
+    String fileLocation;
+    Double cost;
+    String propertyDescription;
+}
