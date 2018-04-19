d54ff97d4131d28b8f46b83578da1a4f31c22bbe
James Luksich <JLUKSICH@amfam.com>
2018-04-19 16:12:50 -0500

    added Vision to Gradle - James

--- ARPropertyPriceCalculator/app/src/main/java/com/amfam/wombat/arpropertypricecalculator/MainActivity.java
+++ ARPropertyPriceCalculator/app/src/main/java/com/amfam/wombat/arpropertypricecalculator/MainActivity.java
@@ -2,6 +2,20 @@ package com.amfam.wombat.arpropertypricecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
